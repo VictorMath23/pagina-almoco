6f97652cea316ea5a42412a743b6045a1f51edd6
Caio Victor Rafael <caio-victor-rafael@hotmail.com>
2023-05-11 15:56:03 -0300

    :sparkles: feat: Criado arquivo de conexao e arquivo functions.js para manipular DOM e receber requisição.

---
+++ database/database.php
@@ -0,0 +1,33 @@
+<?php
+// Configuração do banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "h_almoco";
+
+// Cria a conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica se a conexão foi estabelecida com sucesso
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+// Verifica se foi enviado algum dado através do método POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recupera os valores enviados pelo formulário
+    $linha = $_POST["linha"];
+    $horario = $_POST["horario"];
+
+    // Insere os valores na tabela do banco de dados
+    $sql = "INSERT INTO almoco (linha, horario) VALUES ('$linha', '$horario')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Dados inseridos com sucesso";
+    } else {
+        echo "Erro ao inserir dados: " . $conn->error;
+    }
+}
+
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
