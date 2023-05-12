9116b7475aa7dd3da99e9e991efcca7ed33c305f
Victor Matheus <victormath23@outlook.com>
2023-05-12 09:25:04 -0300

    Pagina da liderança finalizada.

---
+++ database/database.php
@@ -0,0 +1,100 @@
+<?php
+
+
+// Configuração do banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mfg";
+
+// Cria a conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Consulta os horários de almoço
+$query = "SELECT linha, horario, revezamento FROM mfg.almoco_manufatura order by linha asc";
+$result = $conn->query($query);
+
+// Cria um array para armazenar os horários de almoço
+$horarios = array();
+
+if ($result->num_rows > 0) {
+    // Percorre cada linha do resultado da consulta e adiciona ao array de horários
+    while($row = $result->fetch_assoc()) {
+        $horarios[] = array(
+            'linha' => $row['linha'],
+            'horario' => $row['horario'],
+            'revezamento' => $row['revezamento']
+        );
+    }
+}
+
+// Retorna os horários de almoço em formato JSON
+header('Content-Type: application/json');
+echo json_encode($horarios);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $horarios = $_POST['horario'];
+  $revezas = $_POST['reveza'];
+
+  foreach($horarios as $key => $horario){
+    $linha = $_POST['linha'][$key];
+    $reveza = $revezas[$key];
+
+    // Verifica se ocorreu um erro ao conectar
+    if ($conn->connect_error) {
+      die('Erro de conexão (' . $conn->connect_errno . ') '
+      .$conn->connect_error);
+    }
+    
+    // Verifica se a linha já existe na tabela
+    $query = "SELECT COUNT(*) FROM mfg.almoco_manufatura WHERE linha = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('s', $linha);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+
+    if($count == 0){
+      // Se a linha não existir, faz um insert
+      $query = "INSERT INTO mfg.almoco_manufatura (linha, horario, revezamento) VALUES (?, ?, ?)";
+      $stmt =$conn->prepare($query);
+      $stmt->bind_param('sss', $linha, $horario, $reveza);
+      $stmt->execute();
+
+      // Mensagem de validação
+      echo "<script>alert('Linha inserida com sucesso!')</script>";
+     
+      
+    } else {
+      // Se a linha existir, verifica se o revezamento é diferente do valor atual
+      $query = "SELECT revezamento, horario FROM mfg.almoco_manufatura WHERE linha = ?";
+      $stmt = $conn->prepare($query);
+      $stmt->bind_param('s', $linha);
+      $stmt->execute();
+      $stmt->bind_result($atual_revezamento,$atual_horario);
+      $stmt->fetch();
+      $stmt->close();
+      
+      if(($atual_revezamento != $reveza) || ($atual_horario != $horario)) {
+        // Se o revezamento for diferente, faz um update
+        $query = "UPDATE mfg.almoco_manufatura 
+        SET 
+            horario = IF(? = '', horario, ?),
+            revezamento = IF(? = '', revezamento, ?)
+        WHERE linha = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('sssss', $horario, $horario, $reveza, $reveza, $linha);
+        $stmt->execute();  
+        
+        // Mensagem de validação
+        echo "<script>alert('Linha inserida com sucesso!')</script>";
+      }
+      // Se o revezamento for igual, não faz nada
+      
+    }    
+  }
+}
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
---
+++ functions.js
@@ -0,0 +1,51 @@
+// Seleciona todos os botões "submit" na página e adiciona um evento "click" a eles
+var submitButtons = document.querySelectorAll('input[type="submit"]');
+submitButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+        // Impede que o formulário seja enviado normalmente
+        event.preventDefault();
+
+        // Obtém o tr pai do botão clicado
+        var tr = event.target.closest("tr");
+
+        // Obtém os inputs de cada td dentro do tr
+        var timeInput = tr.querySelector('input[name="horario[]"]');
+        var linhaInput = tr.querySelector('input[name="linha[]"]');
+        var revezaSelect = tr.querySelector('select[name="reveza[]"]');
+
+        // Cria um objeto FormData com os valores dos inputs
+        var formData = new FormData();
+        formData.append("horario[]", timeInput.value);
+        formData.append("linha[]", linhaInput.value);
+        formData.append("reveza[]", revezaSelect.value);
+
+        // Faz uma requisição Ajax usando o método POST
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "database/database.php");
+        xhr.onload = function () {
+            // Exibe a resposta da requisição no console
+            console.log(xhr.responseText);
+        };
+        xhr.send(formData);
+
+        // Exibe uma mensagem de sucesso após o envio do formulário
+        alert("Horario da "+ linhaInput.value +" definido com sucesso!");
+
+        var formData = new FormData();
+        formData.append("horario[]", timeInput.value);
+        formData.append("linha[]", linhaInput.value);
+        formData.append("reveza[]", revezaSelect.value);
+
+        // Faz uma requisição Ajax usando o método POST
+        $.ajax({
+            url: "http://localhost/pagina-almoco_/database/database.php",
+            type: "GET",
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("Erro: " + textStatus);
+            }
+        });
+    });
+});
